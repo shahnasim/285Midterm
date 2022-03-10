9b1af329425b53a58c6d2cbf707cd640d6182d66
Nasim Ahmed <hafiznasim493@gmail.com>
2022-03-10 15:21:27 -0500

    commit to master

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,7 +1,7 @@
 public class AddIntegers {
     public static void main(String[] args) {
-        int first = 15;
-        int second = 25;
+        int first = 20;
+        int second = 30;
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
         System.out.println("The sum is: " + sum);
